0999ba68f19fc57b54811e780f8b2b918f5da98c
GrooveStomp <aaron.oman@gmail.com>
2015-03-26 11:46:37 -0700

    Add a helper function to recover keyboard after using VMWare.

--- .bash.d/misc.sh
+++ .bash.d/misc.sh
@@ -1,3 +1,10 @@
 random-sin() {
     xidel -q http://www.fakenamegenerator.com/social-insurance-number.php --css="#details .content p" | awk 'NR == 2' | awk '{$1=$2=""; print $0}' | tr -d ' '
 }
+
+vmware-recover-keyboard() {
+    echo "WARNING! Specific to System76 Galago UltraPro with Kinesis Advantage keyboard."
+    setxkbmap -model pc105 -layout us -option -option ',winkeys' -option 'caps:ctrl-modifier'
+    # The following command shows current settings
+    # setxkbmap -query
+}
