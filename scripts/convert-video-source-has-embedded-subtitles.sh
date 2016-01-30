2b73e87d0dd5b8610f177be561521ffc297a2ed1
GrooveStomp <aaron.oman@gmail.com>
2016-01-30 13:17:03 -0800

    Updated convert video script

--- scripts/convert-video-source-has-embedded-subtitles.sh
+++ scripts/convert-video-source-has-embedded-subtitles.sh
@@ -1,1 +1,9 @@
-ffmpeg -i eps1.2_d3bug.mkv.mp4 -vf subtitles=eps1.2_d3bug.mkv.mp4 -q:a 0 -q:v 0 -acodec ac3 -strict experimental output.mp4
+function convert-video() {
+    if [[ "$num_args" -ne 2 ]] || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
+        echo "Usage: ${FUNCNAME[0]} input_video output_.mp4_file"
+        echo "  eg.: ${FUNCNAME[0]} avengers.mkv avengers.mp4"
+        echo "       -h|--help: This output"
+        return
+    fi
+    ffmpeg -i $1 -vf subtitles=$1 -q:a 0 -q:v 0 -acodec ac3 -strict experimental $2
+}
