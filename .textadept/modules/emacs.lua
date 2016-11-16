bd73f7573360e43113786cc56392e33cf54748a7
GrooveStomp <aaron.oman@gmail.com>
2016-11-15 16:50:33 -0800

    More tweaks - mostly a list of things to do.

--- .textadept/modules/emacs.lua
+++ .textadept/modules/emacs.lua
@@ -1,4 +1,16 @@
--- Use Emacs key bindings.
+-- TODO(AARON):
+-- * M-o doesn't work properly on the first line of the file.
+-- * Write buffer name when saving with "C-x s".
+-- * Implement incremental forward search.
+-- * Implement incremental backward search.
+-- * Fix buggy C-x k closing of views.
+-- * See if it's possible to properly hide all scrollbars all the time.
+-- * Implement C-x 0 to close the current view.
+-- * Implement C-x 1 to close all views other than the current one.
+-- * M-f should go to the end of the word, not the beginning of the next one.
+-- * Don't put closing single or double quote when typing one.
+-- * Don't put closing parenthesis, bracket or brace when typing one.
+
 keys.CLEAR = 'cg'
 
 keys['ca'] = buffer.home
@@ -14,7 +26,10 @@ keys['cf'] = buffer.char_right
 keys['cb'] = buffer.char_left
 keys['cv'] = buffer.page_down
 keys['cd'] = buffer.clear
-keys['ck'] = buffer.del_line_right
+keys['ck'] = function()
+  buffer.del_line_right()
+  buffer.clear()
+end
 
 keys['ag'] = {
     ['ag'] = function()
@@ -25,7 +40,10 @@ keys['ag'] = {
 
 keys['cx'] = {
     ['cc'] = quit,
-    ['cs'] = io.save_file,
+    ['cs'] = function()
+      io.save_file()
+      ui.statusbar_text = 'Saved Buffer' -- TODO(AARON): Get name of buffer that was saved.
+    end,
     ['cw'] = io.save_file_as,
     ['k'] = function()
       local res = view.unsplit(view)
@@ -69,3 +87,5 @@ keys['af'] = buffer.word_right
 keys['ab'] = buffer.word_left
 keys['av'] = buffer.page_up
 keys['ad'] = buffer.del_word_right
+keys['ax'] = function() ui.command_entry.enter_mode('lua_command') end
+
